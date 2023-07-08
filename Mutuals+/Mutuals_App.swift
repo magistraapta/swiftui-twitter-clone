85ad3e170e7aa51aba16b1c8d1ee06330ce8b591
Magistra <magistraagis@gmail.com>
2023-07-08 19:37:41 +0800

    Initial Commit

---
+++ Mutuals+/Mutuals_App.swift
@@ -0,0 +1,17 @@
+//
+//  Mutuals_App.swift
+//  Mutuals+
+//
+//  Created by magistra aptam on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Mutuals_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
